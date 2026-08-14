dd41f6690efa6146a82b722d7ed2658978c4c049
https-aya <138739258+SaitoOmura@users.noreply.github.com>
2026-08-14 10:20:44 +0900

    change

--- GraduationProject/AppMain.cpp
+++ GraduationProject/AppMain.cpp
@@ -1,18 +1,32 @@
 #include "DxLib.h"
+#include "Application.h"
 
-int WINAPI WinMain(_In_ HINSTANCE hInstance,
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPSTR lpCmdLine,_In_ int nShowCmd)
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd)
+{
+	try
 	{
-		ChangeWindowMode(TRUE);
+		// プロジェクトを生成する
+		Application* app = Application::GetInstance();
 
-		if (DxLib_Init() == -1)
-		{
-			return -1;
-		}
-		WaitKey();
-	
-		DxLib_End();
+		// 初期化処理
+		app->WakeUp();
 
-		return 0;
+		// 実行処理
+		app->Run();
+
+		// 終了時処理
+		app->Shutdown();
 	}
+	catch (std::string error_log)
+	{
+		// エラー内容を出力する
+		return ErrorThrow(error_log);
+	}
+
+	// 終了状態を通知する
+	return D_SUCCESS;
+}
